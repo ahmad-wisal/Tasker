3cdd5dd8d8a37f8187e1ff442980fd9a74de9007
Hamza khan <hamzakhan.std25@gmail.com>
2026-05-01 14:56:58 +0500

    added authContext + role base route

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -23,7 +23,7 @@ export const registerUser = async (req, res) => {
       password: hashedPassword,
       role,
     });
-    
+
     res.status(201).json({
       message: "User registered successfully",
       user: {
@@ -45,7 +45,7 @@ export const loginUser = async (req, res) => {
     // check if user exists
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(400).json({ message: "Invalid credentials" });
+      return res.status(400).json({ message: "User not found" });
     }
 
     // compare passwords
@@ -61,11 +61,68 @@ export const loginUser = async (req, res) => {
       { expiresIn: "1d" }
     );
 
+    const userData = {
+      id: user._id,
+      email: user.email,
+      role: user.role,
+    };
+
     res.json({
       message: "Login successful",
       token,
+      user : userData
     });
+
+
+    // res.cookie('token', token, {
+    //   httpOnly: true,
+    //   secure: false, // Set to true in production with HTTPS
+    //   sameSite: 'lax',
+    //   maxAge: 24 * 60 * 60 * 1000 // 1 day
+    // });
+    // res.status(200).json({ message: "Logged in successfully" });
+
+
+
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
+
+
+
+// VERIFY USER (for session persistence)
+
+export const verifyUser = async (req, res) => {
+
+  console.log("verify user is calling...")
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
+  if (!token) {
+    return res.status(401).json({ message: "Not authorized, no token" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // ✅ FETCH FULL USER FROM DATABASE
+    req.user = await User.findById(decoded.id).select("-password");
+
+    if (!req.user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+    res.status(200).json({ user: { id: req.user._id, email: req.user.email, role: req.user.role } });
+  }
+  catch (error) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+
+}
+
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { registerUser, loginUser } from "../controllers/authController.js";
+import { registerUser, loginUser, verifyUser } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/me", verifyUser);
+
+
+
+router.get("/", (req, res) => {
+    res.send("Auth route is working...");
+});
 
 export default router;
